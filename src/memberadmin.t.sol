302b2373f77347232704cc3a51fa50dc496360f9
Jeroen Offerijns <jeroen@offerijns.com>
2021-01-27 09:37:51 +0100

    feat: set up mocks

--- src/memberadmin.t.sol
+++ src/memberadmin.t.sol
@@ -3,19 +3,23 @@ pragma solidity >=0.5.15 <0.6.0;
 import "ds-test/test.sol";
 
 import "./memberadmin.sol";
+import "./memberlist.mock.sol";
 
 contract memberadminTest is DSTest {
     MemberAdmin memberAdmin;
+    MemberlistMock memberlist;
 
     function setUp() public {
         memberAdmin = new MemberAdmin();
+        memberlist = new MemberlistMock();
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
+    function test() public {
+        usr = "0x0";
+        validUntil = now + 365 days;
+        memberAdmin.updateMember(address(memberlist), usr, validUntil)
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+        assertEq(memberlist.calls("updateMember"), 1);
+        assertEq(sub.values_address("updateMember_usr"), usr);
+        assertEq(sub.values_uint("updateMember_validUntil"), validUntil);
     }
-}
---
+++ src/memberlist.mock.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.5.15 <0.6.0;
+
+import "./mock.sol";
+
+contract MemberlistMock is Mock {
+    function updateMember(address usr, uint256 validUntil) public {
+      calls["updateMember"]++;
+      values_address["updateMember_usr"] = usr;
+      values_uint["updateMember_validUntil"] = validUntil;
+    }
+}
